fae96afab4784840b88c92008d2f4305f603fe1e
Aleksandra <93858869+lesssovest@users.noreply.github.com>
2026-04-24 11:58:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/insightful-nudges/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
